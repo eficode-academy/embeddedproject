df2773cf2b2fcaf6d95d368d8298f8c24ade974f
Mike Long <mike@praqma.com>
2017-10-10 10:27:53 +0200

    Simplify circleci build steps and remove duplication

--- build.sh
+++ build.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
 set -x
+set -e
 
 export DEBIAN_FRONTEND=noninteractive
 export WORKSPACE_DIR=/workspace
 # export THIRDPARTY_DIR=${WORKSPACE_DIR}/3rdparty
 export PROJECT_DIR=${WORKSPACE_DIR}/native-app
 export OUTPUT_DIR=${WORKSPACE_DIR}/out
-
-if [ "$PWD" != "$WORKSPACE_DIR" ]; then
-  echo "Build helper script to be run inside a container."
-  echo "Go to $WORKSPACE_DIR directory before running this script."
-  exit 1
-fi
 
 make all
